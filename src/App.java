a61bc5135416ed0d0e705a2d84ee84e8e432df8a
niasalgado <salgado.nia@gmail.com>
2023-11-28 15:47:05 -0500

    WIP: creates troll and ogre event handlers, prints instances to console, and skeleton on compare method

--- src/App.java
+++ src/App.java
@@ -15,9 +15,7 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
@@ -26,12 +24,12 @@ public class App extends Application {
         launch(args);
     }
 
-    public void start(Stage stage) {
-        Weapon axe = new Weapon("axe",5 , "to slice anyone or anything within 10 feet", "cudgel");
-        Weapon bo = new Weapon("bo", 3, "to bludgeon anyone or anything within 10 feet", "staff");
-        Weapon grenade = new Weapon("grenade", 6, "destorys everything within 25 feet of explosion site", "explosive");
-        Weapon sleepSpell = new Weapon("sleepSpell", 5, "puts named person to sleep, must be within 20 feet of initiator.", "spell");
+    Weapon axe = new Weapon("axe",5 , "to slice anyone or anything within 10 feet", "cudgel");
+    Weapon bo = new Weapon("bo", 3, "to bludgeon anyone or anything within 10 feet", "staff");
+    Weapon grenade = new Weapon("grenade", 6, "destorys everything within 25 feet of explosion site", "explosive");
+    Weapon sleepSpell = new Weapon("sleepSpell", 5, "puts named person to sleep, must be within 20 feet of initiator.", "spell");
 
+    public void start(Stage stage) {
         GridPane pane = new GridPane();
 
         pane.setAlignment(Pos.TOP_LEFT);
@@ -39,6 +37,8 @@ public class App extends Application {
         pane.setHgap(20);
         pane.setVgap(20);
 
+        // TODO: add area for the warrior/hero photos above the buttons
+        // heroes and monsters buttons
         Button warriorBtn = new Button("Create Warrior");
         pane.add(warriorBtn, 1, 4);
 
@@ -55,17 +55,22 @@ public class App extends Application {
         trollBtn.setVisible(false);
 
 
+        // compare buttons
         Button compareWarrBtn1 = new Button("Warrior vs Ogre");
         pane.add(compareWarrBtn1, 3, 6);
+        compareWarrBtn1.setVisible(false);
 
         Button compareWarrBtn2 = new Button("Warrior vs Troll");
         pane.add(compareWarrBtn2, 3, 7);
+        compareWarrBtn2.setVisible(false);
 
         Button compareFaerieBtn1 = new Button("Faerie vs Ogre");
         pane.add(compareFaerieBtn1, 3, 8);
+        compareFaerieBtn1.setVisible(false);
 
         Button compareFaerieBtn2 = new Button("Faerie vs Troll");
         pane.add(compareFaerieBtn2, 3, 9);
+        compareFaerieBtn2.setVisible(false);
 
 
         Label message = new Label();
@@ -82,7 +87,7 @@ public class App extends Application {
                 ArrayList<Weapon> weapons = new ArrayList<Weapon>(Arrays.asList(axe, bo, grenade));
 
                 try {
-                    Warrior warrior = new Warrior("Mightus", 5, "healthy", weapons, false);
+                    Warrior warrior = new Warrior("Mulan", 5, "healthy", weapons, false);
                     // TODO: change the SOP line below to display the created instance on the FX stage
                     System.out.println(warrior.getName());
                     warriorBtn.setVisible(false);
@@ -100,7 +105,7 @@ public class App extends Application {
                 ArrayList<Weapon> weapons = new ArrayList<Weapon>(Arrays.asList(sleepSpell));
 
                 try {
-                    Faerie faerie = new Faerie("Lilly", 5, "healthy", weapons, 2.5);
+                    Faerie faerie = new Faerie("Tinkerbell", 5, "healthy", weapons, 2.5);
                     // TODO: change the SOP line below to display the created instance on the FX stage
                     System.out.println(faerie.getName());
                     faerieBtn.setVisible(false);
@@ -111,8 +116,45 @@ public class App extends Application {
             }
         });
 
+        // ogre handler
+        ogreBtn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent e) {
+                ArrayList<Weapon> weapons = new ArrayList<Weapon>(Arrays.asList(bo, grenade));
+                Ogre ogre = new Ogre("Shrek", "swamp", "low", 6, weapons, false);
+                // TODO: change the SOP line below to display the created instance on the FX stage
+                System.out.println(ogre.getName());
+                ogreBtn.setVisible(false);
+                trollBtn.setVisible(true);
+            }
+        });
+
+        // troll handler
+        trollBtn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent e) {
+                ArrayList<Weapon> weapons = new ArrayList<Weapon>(Arrays.asList(bo, grenade));
+                Troll troll = new Troll("Mike", "office", "low", 2, weapons, 3.0);
+                // TODO: change the SOP line below to display the created instance on the FX stage
+                System.out.println(troll.getName());
+                trollBtn.setVisible(false);
+
+                compareWarrBtn1.setVisible(true);
+                compareWarrBtn2.setVisible(true);
+                compareFaerieBtn1.setVisible(true);
+                compareFaerieBtn2.setVisible(true);
+
+                compares();
+            }
+        });
+
         stage.setTitle("My FX App");
         stage.setScene(scene);
         stage.show();
+    }
+
+    // TODO: compares heros vs monster depending on what button the user clicks
+     public void compares() {
+        System.out.println("comapres");
     }
 }
